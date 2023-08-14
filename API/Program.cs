da81e5513c1c7efc7bcef973cbf71518c7abe0c4
Ian <Ian.Pilsbury@quikrete.com>
2023-08-14 14:10:57 -0400

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,34 +1,38 @@
-// Creates Kestrel Server
 using Microsoft.EntityFrameworkCore;
+using Domain;
+using Persistence;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// used for dependency injections
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<Persistence.DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("defaultConnection"));
 });
 
+builder.Services.AddCors(opt => 
+{
+    opt.AddPolicy("CorsPolicy", policy => 
+    {
+        policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+    });
+});
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline. Think of this as middleware
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseAuthorization();
+// Use CORS middleware before other middleware
+app.UseCors("CorsPolicy");
 
-// Registers endpoints with controllers
+app.UseAuthorization();
 app.MapControllers();
 
-// using means whatever is inside the scope will be destroyed after the service is done being used. 
 using var scope = app.Services.CreateScope();
 var services = scope.ServiceProvider;
 
@@ -38,7 +42,7 @@ try {
     await Persistence.Seed.SeedData(context);
 } catch (Exception ex) {
     var logger = services.GetRequiredService<ILogger<Program>>();
-    logger.LogError(ex, "An error occured during migration");
-};
+    logger.LogError(ex, "An error occurred during migration");
+}
 
 app.Run();
