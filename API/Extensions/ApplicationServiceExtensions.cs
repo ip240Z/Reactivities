31ab4988aaa2a9d41ff34f7ede89787dddc3aed6
Ian <Ian.Pilsbury@quikrete.com>
2023-08-21 08:20:27 -0400

    End of section 4

---
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,32 @@
+
+using Application.Core;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config) {
+                        
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            services.AddDbContext<Persistence.DataContext>(opt => {
+                opt.UseSqlite(config.GetConnectionString("defaultConnection"));
+            });
+
+            services.AddCors(opt => 
+            {
+                opt.AddPolicy("CorsPolicy", policy => 
+                {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
+
+            services.AddMediatR(typeof(Application.Activities.List.Handler));
+            services.AddAutoMapper(typeof(MappingProfiles).Assembly);
+
+            return services;
+        }
+    }
+}
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -25,7 +25,6 @@ function App() {
           </List.Item>
         ))}
       </List>
-      <Button/>
     </div>
   );
 }
